2cb1f6c0735db6bb749a4c6211580183bbb23258
subash338 <subashrajendran77@gmail.com>
2024-11-10 12:47:44 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var typed= new Typed(".text",{
+    strings: ["Workplace Safety And Health Coordinator"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
+
+
+
